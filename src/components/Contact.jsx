d67a7f09bdcf99cd2cd8e060c9bcddf0610f74d7
gwatsin <gwatsinthong@gmail.com>
2025-05-23 12:56:17 +0530

    story section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div id="#contact" className="my-20 min-h-96 w-screen px-10">
+      <div className="relative">
+
+      </div>
+    </div>
+  )
+}
+
+export default Contact
