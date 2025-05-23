cf66bc10bcb745a1ceb645930567264b0896368e
gwatsin <gwatsinthong@gmail.com>
2025-05-23 16:16:59 +0530

    button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
-  return (
-    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full px-7 py-3 text-black ${containerClass}`}>
-        {leftIcon}
+  const [animateText, setAnimateText] = useState(false)
 
-        <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
-            <div>
-                {title}
-            </div>
-        </span>
-        {rightIcon}
+  const triggerAnimation = () => {
+    setAnimateText(false)
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        setAnimateText(true)
+      })
+    })
+  }
+
+  return (
+    <button
+      id={id}
+      onMouseEnter={triggerAnimation}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden bg-violet-50 px-7 py-3 text-black 
+        font-general text-xs uppercase transition-all duration-200 ease-[cubic-bezier(0.65,0,0.35,1)] 
+        rounded-3xl 
+        [clip-path:polygon(0%_0%,_100%_0%,_100%_100%,_0%_100%)] 
+        hover:[clip-path:polygon(0%_0%,_98%_4%,_100%_100%,_2%_100%)] 
+        hover:rounded-xl
+        ${containerClass}`}
+    >
+      {leftIcon}
+
+      <span className="relative inline-flex overflow-hidden">
+        <div className={animateText ? 'bounce-once' : ''}>
+          {title}
+        </div>
+      </span>
+
+      {rightIcon}
     </button>
   )
 }
---
+++ src/components/SvgClipPaths.jsx
@@ -0,0 +1,12 @@
+export const GlobalClipPaths = () => (
+  <svg width="0" height="0" style={{ position: 'absolute' }} aria-hidden="true">
+    <defs>
+      <clipPath id="clip-default" clipPathUnits="objectBoundingBox">
+        <polygon points="0 0, 1 0, 1 1, 0 1" />
+      </clipPath>
+      <clipPath id="clip-hover" clipPathUnits="objectBoundingBox">
+        <polygon points="0.02 0.14, 0.92 0.25, 0.96 0.93, 0.1 0.93" />
+      </clipPath>
+    </defs>
+  </svg>
+)
